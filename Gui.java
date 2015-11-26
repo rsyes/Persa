27f6f6b8b1a2c1f050bf67a562d2bb86d0a1fd21
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-11-26 02:10:29 -0500

    Gui Version No 1 02:10 am

---
+++ Gui.java
@@ -0,0 +1,10 @@
+package vista;
+	import javax.swing.JButton;
+	import javax.swing.JPanel;
+	public class Gui extends JPanel {
+			JButton b1;
+			public Gui(){
+					b1= new JButton("Cielo");
+					add(b1);
+				}
+		}
