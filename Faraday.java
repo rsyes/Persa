eab47d5ce44ffc0308dee507f8ad1ad89bc6c32b
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-11-26 02:33:02 -0500

    Update Faraday.java

--- Faraday.java
+++ Faraday.java
@@ -1,10 +1,21 @@
 package modelo;
+	import javax.swing.JFrame;
+	import modelo.Logica;
+	import vista.Gui;
 	public class Faraday extends JFrame{
 		Logica datos;
 		Gui interfaz;
 		public Faraday(){
+			super("daruedar.com")
 			datos= new Logica();
-			Gui interfaz=new Interfaz();
+			Gui interfaz=new Gui();
+			add(interfaz);
+			setSize(350,250);
+			setVisible(true);
+		}
+		public static void main(String[] args){
+			Faraday app= new Faraday();
+			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}
 		
 	}
