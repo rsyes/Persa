a3c1b858d1359ff860bd6a151635a681bfcc2509
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-11-26 04:26:41 -0500

    Metodo para crear la primera tabla
    Funciona A la perfeccion probado ydepurado a las 04:26 am

--- Query.java
+++ Query.java
@@ -7,10 +7,25 @@ import java.sql.SQLException;
 
 /**Sirve para ejecutar las Query que debemos implementar*/
 public class Query {
+	
 	private static BaseDeDatos base;
-
+	Connection c;
+	java.sql.Statement stmt;
+	
+	public Query(){
+		c=null;
+		stmt=null;
+		base= new BaseDeDatos();
+		ResultSet rs;
+		rs=null;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Query app= new Query();
+		app.crearTablaPrecios();
+		
+		/*
 		Connection c = null;
 	    java.sql.Statement stmt = null;
 	    base= new BaseDeDatos();
@@ -34,7 +49,7 @@ public class Query {
 			//stmt.executeUpdate(sql);//Una vez creada las tablas no debo crear mas
 			//stmt.executeUpdate(sql2);// sentencia insert
 			  
-			/*Recuperando Informacion de  una tabla*/
+			//Recuperando Informacion de  una tabla
 				rs = stmt.executeQuery( "SELECT * FROM Precios;" );
 				while ( rs.next() ) {
 		         int id = rs.getInt("id");
@@ -49,7 +64,7 @@ public class Query {
 		         System.out.println( "Price = " + Price );
 		         System.out.println();
 		      }
-			/*Finalizacion del proceso de extraer la informacion de una Tabla*/	
+			//Finalizacion del proceso de extraer la informacion de una Tabla	
 		      
 		      
 		      stmt.close();
@@ -60,7 +75,31 @@ public class Query {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+*/
 	}
+	
+	public void crearTablaPrecios(){
+		try{
+				c=base.conectarA("TonyK.db");
+				stmt = c.createStatement();
+				String sql = "CREATE TABLE Precios " +
+                   "(Id INT PRIMARY KEY     NOT NULL, AUTO_INCREMENT," +
+                   " Nombre           TEXT    NOT NULL, " + 
+                   //" AGE            INT     NOT NULL, " + 
+                   //" ADDRESS        CHAR(50), " + 
+                   " Precio         REAL)"; 
+				String sql2 = "INSERT INTO Precios (Nombre,Precio) " +
+                   "VALUES ('Zanahoria',  3.50 );"; 
+				stmt.executeUpdate(sql);
+				stmt.close();
+				c.close();
+				
+			}
+		catch(SQLException e){
+				e.printStackTrace();
+			}
+	}
+	
+	
 
 }
