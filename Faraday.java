18a582b5d6272635db81ab529132a30db90302d9
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-11-27 15:39:05 -0500

    Correccion Singleton
    EL ARCHIV O TOTAL ESTA EN HOTMAIL, fue el que trabajo desde las 11 am

--- Faraday.java
+++ Faraday.java
@@ -1,24 +1,51 @@
 package controller;
+	import java.util.ArrayList;
+	import javax.swing.DefaultListModel;
 	import javax.swing.JFrame;
+	import javax.swing.ListModel;
 	import modelo.Logica;
+	import modelo.Query;
 	import vista.Gui;
-	public class Faraday extends JFrame{
-			Logica datos;
-			Gui interfaz;
-			private static Faraday instance= new Faraday();
+	public class Faraday extends JFrame{		
+			Gui interfaz=new Gui();
+			Logica datos= new Logica();
+			private static Faraday instance;
 			private Faraday(){
 					super("daruedar.com");
-					datos= new Logica();
 					interfaz=new Gui();
 					add(interfaz);
+					ArrayList<String> t=Query.getNombres();
+					ArrayList<Double> u=Query.getPrecios();
+					ArrayList<String> v=Query.getPlatos();
+					for(String s:t){
+						interfaz.addtoModelNombres(s);
+					}
+					for(Double d:u){
+						interfaz.addtoModelPrecios(d);
+					}
+					//for(String s:v){
+						//interfaz.addtoModelPlatos(s);
+					//}
 					setSize(350,250);
 					setVisible(true);
 				}
+			public Gui getInterfaz() {
+				return interfaz;
+			}
 			public static void main(String[] args){
-					Faraday app=null;
-					app.getInstance().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					Faraday app=Faraday.getInstance();
+					app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				}
 			public static Faraday getInstance() {
+				if(instance==null){instance= new Faraday();}
 				return instance;
 			}
-		}
+			public void tome(ArrayList<String> nombres) {
+				// TODO Auto-generated method stub
+				for(String s:nombres){
+					
+				}
+				
+			}
+		
+}
