5ff2905392f99145f87c9ca1a09fd8ba31e645c2
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-11-26 22:04:01 -0500

    Implementacion Singleton en el Controlador
    De paso corregi las dos instancias que aparecian , ocacionadas porque
    declarava el constructor como privado pero
    yo mismo violaba la regla  usando el operador new ene l metodo main
    sSolucion declarar la variable tipo Faraday como null en lugar de usar el new
    y en la siguiente linea pedir la instancia de la clase

--- Faraday.java
+++ Faraday.java
@@ -1,21 +1,24 @@
-package modelo;
+package controller;
 	import javax.swing.JFrame;
 	import modelo.Logica;
 	import vista.Gui;
 	public class Faraday extends JFrame{
-		Logica datos;
-		Gui interfaz;
-		public Faraday(){
-			super("daruedar.com")
-			datos= new Logica();
-			Gui interfaz=new Gui();
-			add(interfaz);
-			setSize(350,250);
-			setVisible(true);
+			Logica datos;
+			Gui interfaz;
+			private static Faraday instance= new Faraday();
+			private Faraday(){
+					super("daruedar.com");
+					datos= new Logica();
+					interfaz=new Gui();
+					add(interfaz);
+					setSize(350,250);
+					setVisible(true);
+				}
+			public static void main(String[] args){
+					Faraday app=null;
+					app.getInstance().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				}
+			public static Faraday getInstance() {
+				return instance;
+			}
 		}
-		public static void main(String[] args){
-			Faraday app= new Faraday();
-			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		}
-		
-	}
